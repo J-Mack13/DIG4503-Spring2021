99569804c92fbf0dec3d83ac7649583d0e8915e9
JMurphy98 <jmmurphy137@gmail.com>
2021-01-22 14:14:15 -0500

    Lab1 done

---
+++ lab1/Julia.js
@@ -0,0 +1,8 @@
+import Person from './Person.js';
+
+class Julia extends Person {
+  constructor(){
+    super("Julia","#00A0B0");
+  }
+}
+export default Julia;
---
+++ lab1/index.js
@@ -0,0 +1,4 @@
+import Julia from './Julia.js';
+
+const p = new Julia ("Julia","#00A0B0");
+p.speak();
