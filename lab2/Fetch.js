3b6e3fba49878e535b4ef2a1f28a7c82b20d740b
JMurphy98 <jmmurphy137@gmail.com>
2021-01-29 19:20:40 -0500

    Things are starting to work!

--- lab2/Fetch.js
+++ lab2/Fetch.js
@@ -8,11 +8,11 @@ class Fetch {
   }
   fetch(){
     axios ('https://pokeapi.co/api/v2/pokemon/' + this.pokemon)
-      .then(function (response) {
+      .then((response) => {
         const pokemon = response.data;
         console.log(chalk.hex(this.color)("This is a " + pokemon.name + " and its ID is " + pokemon.id));
       })
-    .catch(function (error) {
+    .catch((error) => {
       console.log(chalk.red("Error: " + error));
   });
   } 
