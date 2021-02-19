14f2310381123550cd7ab361d236ab7e44679136
JMurphy98 <jmmurphy137@gmail.com>
2021-02-19 13:32:54 -0500

    Lab5 Gotta Catch Them All!!

---
+++ lab5/src/App.js
@@ -0,0 +1,15 @@
+import { useState } from 'react';
+import Search from './component/Search/index.js';
+import Axios from 'axios';
+
+
+function App () {
+  return (
+    <div>
+      <Search/>
+    </div>
+  );
+}
+
+
+export default App;
