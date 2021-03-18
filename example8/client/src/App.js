3e563ff96e6f542ca99a1a3a62829034202854f7
JMurphy98 <jmmurphy137@gmail.com>
2021-03-18 12:01:08 -0400

    Somethings are still not working

--- example8/client/src/App.js
+++ example8/client/src/App.js
@@ -1,11 +1,16 @@
 import './App.css';
 import Axios from 'axios';
+import { useState } from 'react';
 
 
 function App() {
 
-  const putColor = () =>{
-    Axios.put("http://localhost:45030/color/aqua")
+  const [additionalColor, setAdditionalColor] = useState("");
+  const [searchColor, setSearchColor] = useState("");
+  const [searchResults, setSearchResults] = useState([]);
+
+  const putColor = (color) =>{
+    Axios.put("http://localhost:45030/colors/" + color)
     .then(response => {
       console.log(response.data);
     })
@@ -14,18 +19,19 @@ function App() {
     });
   }
 
-  const getColors = () =>{
-    Axios.put("http://localhost:45030/search/a")
+  const getColors = (color) =>{
+    Axios.put("http://localhost:45030/search/a" + color)
     .then(response => {
       console.log(response.data);
+      setSearchResults(response.data.color)
     })
     .catch(error => {
       console.log("error " + error);
     });
   }
 
-  const deleteColor = () =>{
-    Axios.put("http://localhost:45030/colors/aqua")
+  const deleteColor = (color) =>{
+    Axios.put("http://localhost:45030/colors/aqua" + color)
     .then(response => {
       console.log(response.data);
     })
@@ -38,8 +44,15 @@ function App() {
 
   return (
     <div className="App">
-      <button onClick={() => putColor()}>Click me to test</button>
-      <button onClick={() => getColors()}>Click me to test</button>
+      <input type="text" onChange={(event) => setAdditionalColor(event.target.value)} />
+      <button onClick={() => putColor(additionalColor)}>Click me to test</button>
+      <input type="text" onChange={(event) => setSearchColor(event.target.value)} />
+      <button onClick={() => getColors(searchColor)}>Click me to test</button>
+      {
+        searchResults.map((value, index) => {
+          return <p>{value}</p>
+        })
+      }
       <button onClick={() => deleteColor()}>Click me to test</button>
     </div>
   );
