d706ec0d46da1d740b1d65d9dd6cd0625c826526
JMurphy98 <jmmurphy137@gmail.com>
2021-04-02 10:33:07 -0400

    HELP ME I AM DYING!

---
+++ lab10/index.js
@@ -0,0 +1,26 @@
+import Express from 'express';
+import Database from './Database.js';
+
+const App = Express();
+const port = 45030;
+
+App.use(Express.json());
+
+App.put("/people/:create", (req, res) => {
+  res.json(
+    {
+      create: req.params.create,
+      body: req.body
+    }
+  );
+});
+
+App.get("/people/:person", (req, res) => {
+  res.json(
+    {person: "Not Found!"}
+  )
+})
+
+App.listen(port, () => {
+    console.log("The Server Is Running!");
+});
