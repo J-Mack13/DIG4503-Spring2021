a0cb13d237708e5dfd3288e4d48851e482e417ea
JMurphy98 <jmmurphy137@gmail.com>
2021-04-03 21:56:47 -0400

    I am done with this semester ~_~

--- lab10/Database.js
+++ lab10/Database.js
@@ -3,8 +3,45 @@ import MongoClient from 'mongodb';
 const url = "mongodb+srv://JuliaMurphy:KAJS04R3VmTPtaw3@cluster0.yuzwq.mongodb.net";
 
 class Database {
+    constructor(){
+        this.connection = null;
+        this.database = null;
+        this.collection = null;
+    }
 
+    async connect() {
+        this.connection = await MongoClient.connect(url, {useUnifiedTopology: true});
+        this.database =this.connection.db("lab10");
+        this.collection =this.database.collection("people");
+    }
+
+    async createOne(firstName, lastName, favoriteColor) {
+        let obeject = {
+            "firstName": firstName,
+            "lastName": lastName,
+            "favoriteColor": favoriteColor
+        };
+        if(this.collection !=null) {
+            return await this.collection.insertOne(obeject);
+        }
+    }
+
+    async readOne(query) {
+        let results = await this.collection.findOne({firstName: query});
+        if (results !=null) {
+            return results;
+        }
+        else{
+            return{ person: "not found" };
+        }
+    }
+
+    //NOTE TO SLEF: dont forget to close!!
+    close() {
+        if(this.collection !=null) {
+            this.connection.close();
+        }
+    }
 }
-      });
 
 export default Database;
--- lab10/index.js
+++ lab10/index.js
@@ -2,25 +2,35 @@ import Express from 'express';
 import Database from './Database.js';
 
 const App = Express();
-const port = 45030;
+//!! My sever would not run but once I made it into 45031 it worked! I hope it is ok that I changed it. 
+const port = 45031;
+const d = new Database();
 
 App.use(Express.json());
 
+//create
 App.put("/people/:create", (req, res) => {
-  res.json(
-    {
-      create: req.params.create,
-      body: req.body
-    }
-  );
+  let person = req.body;
+  let result = d.readOne(person);
+  res.json(result);
 });
 
+//read
 App.get("/people/:person", (req, res) => {
-  res.json(
-    {person: "Not Found!"}
-  );
+  let person = req.params.person;
+  let result = d.readOne(person);
+  res.json(result);
 });
 
-App.listen(port, () => {
+
+//test
+App.listen(port, function() {
     console.log("The Server Is Running!");
 });
+
+//Insomnia Test Notes: 1)Yay the sever is running in teminal, time to check Insomnia; 
+//                     2)Something is wrong with "found.One" ~Solution! i miss spelled it fml;
+//                     3)Something is still wrong with "foundOne" ~Solution! it was supposed to be "findOne" + i need more cofffee;
+//                     4)Something is still wrong with "findOne" idk what though, NOTE this is the ERROR: "Cannot read property 'findOne' of null"
+//                       4.1)It does NOT involve lines 18-23 i think
+//                       4.2)Maybe I am testing my code wrong in Insomnia?
